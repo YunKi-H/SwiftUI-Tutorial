e36214ed8038c1a73385f443b0710fd6dc9183ba
maemi <maemi@maemiuicBookPro.com>
2021-12-05 15:24:14 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 매미 on 2021/12/05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
